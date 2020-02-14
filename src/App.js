d053ccbf3649ec480d5193da846f7014678ab171
lee5031207 <lee5031207@naver.com>
2020-02-14 16:05:44 +0900

    완성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
 import axios from "axios";
 import Movie from "./Movies";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends React.Component{
   state = {
@@ -28,20 +31,27 @@ class App extends React.Component{
   render(){
     const { isLoading, movies } = this.state;
     return (
-      <div>
+      <section className="container">
         {isLoading
-          ? "Loading..."
-          : movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-              />
-            ))}
-      </div>
+          ? <div className="loader">
+              <span className="loader_text">LOADING...</span>
+          </div>
+          : <div className="movies row">
+              {movies.map( x=>(
+                <Movie
+                key={x.id}
+                id={x.id}
+                year={x.year}
+                title={x.title}
+                summary={x.summary}
+                poster={x.medium_cover_image}
+                genres={x.genres}
+                rating={x.rating}
+                />
+              ))}
+            </div>
+            }
+      </section>
     );
   }
 }
--- src/Movies.js
+++ src/Movies.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import "./Movie.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
 
-
-function Movie({id,year,title,summary,poster}){
+function Movie({id,year,title,summary,poster,genres,rating}){
     return (
-        <div>
-            <h4>{title}({year})</h4>
-            <img src={poster}></img>
+        <div className="movie col-md-5">   
+            <img src={poster} alt={title} title={title}></img>
+            <div className="movie_data">
+                <p className="movie_title">{title}</p>
+                <p className="movie_year">{year}</p>
+                <p className="genre">{genres[0]}&nbsp;&nbsp;&nbsp;</p>
+                <p className="genre">{genres[1]}&nbsp;&nbsp;&nbsp;</p>
+                <p className="genre">{genres[2]}&nbsp;&nbsp;&nbsp;</p>
+                <p className="genre">{genres[3]}&nbsp;&nbsp;&nbsp;</p>
+            </div>
+            <p className="movie_summary">{summary.slice(0,150)}
+                ...<a href="">more</a>
+            </p>
         </div>
     )
 }
@@ -16,10 +28,15 @@ Movie.propTypes = {
     year : PropTypes.number.isRequired,
     title : PropTypes.string.isRequired,
     summary : PropTypes.string.isRequired,
-    poster : PropTypes.string.isRequired
+    poster : PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
+
+/*{genres.map(genre =>(
+    <p className="genre">{genre}&nbsp;&nbsp;&nbsp;&nbsp;</p>
+    ))}*/
 
 
 
